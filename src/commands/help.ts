afd2180f47ff4fa6b63cdf64202c913e0e79b9cb
Lucas Boje <lucasaboje@gmail.com>
2022-06-11 12:18:56 -0700

    Create help command

---
+++ src/commands/help.ts
@@ -0,0 +1,68 @@
+import { Client, BaseCommandInteraction, GuildMember } from "discord.js";
+import { Command } from "../command";
+import { Channel } from "../types/channel";
+import * as channelQuery from "../models/channel";
+import { BasicUser } from "../types/user";
+import * as bondQuery from "../models/bond";
+import { QueryError } from "mysql2";
+
+export const ListBonds: Command = {
+    name: "help",
+    description: "Learn about command(s)",
+    type: "STRING",
+    options: [
+        {
+            name: "command",
+            description: "Command you want details for",
+            type: "STRING",
+            required: false
+        } as ApplicationCommandOption
+    ],
+    run: async (client: Client, interaction: BaseCommandInteraction) => {
+        channelQuery.get(interaction.channelId, (error: QueryError | null, channel: Channel) => {
+            if (error) {
+                interaction.followUp({
+                    ephemeral: false,
+                    content: "Error accessing database"
+                });
+            } else if (!channel) {
+                interaction.followUp({
+                    ephemeral: false,
+                    content:
+                        "This channel is not connected to a telepathy network, use a connected channel to view bonds."
+                });
+            } else {
+                bondQuery.getAllBondedUsers(
+                    { userid: interaction.user.id },
+                    channel.network,
+                    (error: QueryError | null, users: BasicUser[]) => {
+                        let userIDs: string[] = [];
+                        for (let user of users) {
+                            userIDs.push(user.userid);
+                        }
+                        if (interaction.guild) {
+                            interaction.guild.members.fetch({ user: userIDs }).then((members) => {
+                                let content = "Bonds: ";
+                                for (let member of members) {
+                                    content += "<@" + (member[1].id) + ">, ";
+                                }
+
+                                content = content.slice(0, -2);
+
+                                interaction.followUp({
+                                    ephemeral: false,
+                                    content
+                                });
+                            });
+                        } else {
+                            interaction.followUp({
+                                ephemeral: false,
+                                content: "Error accessing guild"
+                            });
+                        }
+                    }
+                );
+            }
+        });
+    }
+};
